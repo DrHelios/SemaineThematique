35c58c7f135d001930c5b1fe3817999c94123dba
Romain Rebecchi <romain.rebecchi@gmail.com>
2019-10-15 11:53:40 +0200

    onsaijamai

--- Unity/Assets/Scripts/PongScript/Struc/BotStruc.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public struct BotStruc
-{
-    public Vector2 pos;
-    public Vector2 speed;
-}
--- Unity/Assets/Scripts/PongScript/Struc/BulletStruc.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public struct BulletStruc
-{
-    public Vector2 pos;
-    public Vector2 speed;
-}
