177c50a07872ee87bd9420dc8c171f177d60f71b
Thefirenight <alexandre.laberche@gmail.com>
2019-10-16 13:58:59 +0200

    ajout game over

---
+++ Unity/Assets/Scripts/ApplicationData.cs
@@ -0,0 +1,4 @@
+﻿public static class ApplicationData
+{
+ public static string gameOverText;
+}
---
+++ Unity/Assets/Scripts/GameOverText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverText : MonoBehaviour
+{
+    public Text changetext;
+
+    private void Update()
+    {
+        changetext.text = ApplicationData.gameOverText;
+    }
+}
