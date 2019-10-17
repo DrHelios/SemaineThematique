f984b22a982fc05601488d32f470b293254d7a10
Benoit <benoitremy74@gmail.com>
2019-10-17 15:28:47 +0200

    Update scene game over

--- Unity/Assets/Scripts/ReturnToMainMenu.cs
+++ Unity/Assets/Scripts/ReturnToMainMenu.cs
@@ -4,8 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class ReturnToMainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        ShowMouseCursor();
+    }
+
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("Menu");
+    }
+
+    public void ShowMouseCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
