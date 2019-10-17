2b077f0e78a14a4487a439715ee03004b48591a3
QuentinB89 <35403318+QuentinB89@users.noreply.github.com>
2019-10-17 12:31:29 +0200

    Nouveaux prefabs (que j'avais caché)

---
+++ Unity/Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
